8ebee5bebd92af4a7400e8819ee147f423563fb1
Michael Stoltzfus <michael@landiscomputer.com>
2020-09-29 15:31:10 -0400

    Fixed variable name in content.js

--- content.js
+++ content.js
@@ -2,6 +2,6 @@
 
 var callAlert = document.getElementsByClassName('toast-alert');
 
-if(typeof(callAlert) != 'undefined' && alert !=null){
+if(typeof(callAlert) != 'undefined' && callAlert !=null){
     chrome.tabs.sendMessage(tab.id, msg);
 }
